70a7b9f3728d70de4b69ab57db8b02771b9d5e45
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 17:01:05 -0800

    Update portapp.js

--- portapp.js
+++ portapp.js
@@ -1,9 +1,1 @@
-((function(){
-	var a,b,c;
-  c=prompt("Please provide a proxy link to go to (don't forget the https:// or http://!)");
-  b=document.createElement("iframe");
-  b.setAttribute("src",c),b.setAttribute("id","myohthegod");
-  b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; top: 0px; left: 0px; border: 0;");
-  a=document.getElementsByTagName("body")[0];
-  a.appendChild(b)
-})).call(this)
+javascript:((function(){var a,b,c;c=prompt("Please provide a proxy link to go to (don't forget the https:// or http://!)"),b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","myohthegod"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; top: 0px; left: 0px; border: 0;"),a=document.getElementsByTagName("body")[0],a.appendChild(b)})).call(this)
