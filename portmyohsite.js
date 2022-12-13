2ddcc4731d55edcab3a809b6abf9b9d7d111d98a
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 17:08:06 -0800

    Create portmyohsite.js

---
+++ portmyohsite.js
@@ -0,0 +1,1 @@
+javascript:((function(){var a,b,c;c="https://myohthegod.edjiang1234.repl.co",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","myohthegod"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; top: 0px; left: 0px; border: 0;"),a=document.getElementsByTagName("body")[0],a.appendChild(b)})).call(this)
