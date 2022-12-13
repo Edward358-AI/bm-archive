974efdaa86b9da3f5b7b7cec36b6c57644a42d82
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 16:57:24 -0800

    Update gdocs.js

--- gdocs.js
+++ gdocs.js
@@ -1,9 +1,1 @@
-function e(){
-	var e=document.querySelector("link[rel*='icon']")||document.createElement("link");
-  e.type="image/x-icon";
-  e.rel="shortcut icon";
-  e.href="https://ssl.gstatic.com/docs/documents/images/kix-favicon7.ico";
-  document.title="Google Docs";
-  document.getElementsByTagName("head")[0].appendChild(e)
- }
- e(),setInterval(e,1e3)}();
+javascript:void function(){function e(){var e=document.querySelector("link[rel*='icon']")||document.createElement("link");e.type="image/x-icon",e.rel="shortcut icon",e.href="https://ssl.gstatic.com/docs/documents/images/kix-favicon7.ico",document.title="Google Docs",console.log(document.title),document.getElementsByTagName("head")[0].appendChild(e)}e(),setInterval(e,1e3)}();
