fc65d3cb36438cd0ab203b1cc4926d902a4d3f65
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 17:14:42 -0800

    Create shootythingy.js

---
+++ shootythingy.js
@@ -0,0 +1,1 @@
+javascript:void function(){var t=document.createElement("script");t.type="text/javascript",t.src="https://myohthegod.edjiang1234.repl.co/js/shooter.js",document.body.appendChild(t)}();
