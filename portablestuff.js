8794f3128cccd696109d143ac3ee91e750d48835
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 17:13:42 -0800

    Update portablestuff.js

--- portablestuff.js
+++ portablestuff.js
@@ -1,8 +1,13 @@
+/* Portable, utility tools to enhance your browsing experience on the web! */
+
 // MyohTheGod's website (portable)
 javascript:((function(){var a,b,c;c="https://myohthegod.edjiang1234.repl.co",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","myohthegod"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; top: 0px; left: 0px; border: 0;"),a=document.getElementsByTagName("body")[0],a.appendChild(b)})).call(this)
+
 // MyohTheWeb proxy (portable)
 javascript:((function(){var a,b,c;c="https://r3alityweb.herokuapp.com",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","myohthegod"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; top: 0px; left: 0px; border: 0;"),a=document.getElementsByTagName("body")[0],a.appendChild(b)})).call(this)
+
 // Open any link in an iframe (must have FrameKiller off)
 javascript:((function(){var a,b,c;c=prompt("Please provide a proxy link to go to (don't forget the https:// or http://!)"),b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","myohthegod"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; top: 0px; left: 0px; border: 0;"),a=document.getElementsByTagName("body")[0],a.appendChild(b)})).call(this)
+
 // Close anything of the bookmarklets mentioned above
 javascript:void function(){var e=document.getElementById("myohthegod");e.remove()}();
