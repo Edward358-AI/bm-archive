9dd10ffe97513547cbcc4ccf0175ec5e2601b6af
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 17:12:54 -0800

    Update and rename gclass.js to individualcloaks.js

---
+++ individualcloaks.js
@@ -0,0 +1,13 @@
+/* Individual, utility tab cloakers */
+
+// 'To-do' classroom disguise
+javascript:void function(){function e(){var e=document.querySelector("link[rel*='icon']")||document.createElement("link");e.type="image/x-icon",e.rel="shortcut icon",e.href="https://ssl.gstatic.com/classroom/favicon.png",document.title="To-do",console.log(document.title),document.getElementsByTagName("head")[0].appendChild(e)}e(),setInterval(e,1e3)}();
+
+// 'Classes' classroom disguise
+javascript:void function(){function e(){var e=document.querySelector("link[rel*='icon']")||document.createElement("link");e.type="image/x-icon",e.rel="shortcut icon",e.href="https://ssl.gstatic.com/classroom/favicon.png",document.title="Classes",console.log(document.title),document.getElementsByTagName("head")[0].appendChild(e)}e(),setInterval(e,1e3)}();
+
+// Google Docs
+javascript:void function(){function e(){var e=document.querySelector("link[rel*='icon']")||document.createElement("link");e.type="image/x-icon",e.rel="shortcut icon",e.href="https://ssl.gstatic.com/docs/documents/images/kix-favicon7.ico",document.title="Google Docs",console.log(document.title),document.getElementsByTagName("head")[0].appendChild(e)}e(),setInterval(e,1e3)}();
+
+// Google Drive
+javascript:void function(){function e(){var e=document.querySelector("link[rel*='icon']")||document.createElement("link");e.type="image/x-icon",e.rel="shortcut icon",e.href="https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png",document.title="My Drive - Google Drive",console.log(document.title),document.getElementsByTagName("head")[0].appendChild(e)}e(),setInterval(e,1e3)}();
