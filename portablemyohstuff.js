6a0cd0a6eeb7a49240d4fd47ee1dbccbf8257f98
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 17:10:09 -0800

    Update and rename portmyohsite.js to portablemyohstuff.js

---
+++ portablemyohstuff.js
@@ -0,0 +1,6 @@
+// MyohTheGod's website (portable)
+javascript:((function(){var a,b,c;c="https://myohthegod.edjiang1234.repl.co",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","myohthegod"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; top: 0px; left: 0px; border: 0;"),a=document.getElementsByTagName("body")[0],a.appendChild(b)})).call(this)
+// MyohTheWeb proxy (portable)
+javascript:((function(){var a,b,c;c="https://r3alityweb.herokuapp.com",b=document.createElement("iframe"),b.setAttribute("src",c),b.setAttribute("id","myohthegod"),b.setAttribute("style","position: fixed; z-index: 999999; width: 100%; height: 100%; top: 0px; left: 0px; border: 0;"),a=document.getElementsByTagName("body")[0],a.appendChild(b)})).call(this)
+// Close anything 'myohthegod' related
+javascript:void function(){var e=document.getElementById("myohthegod");e.remove()}();
