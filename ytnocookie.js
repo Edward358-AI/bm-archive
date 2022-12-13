cb767bb27c2428861231336cdd9469c84733b58a
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 16:53:38 -0800

    Update ytnocookie.js

--- ytnocookie.js
+++ ytnocookie.js
@@ -1,1 +1,1 @@
-(function () {if (window.location.toString().includes('www.youtube.com/watch?v')) { window.open('https://www.youtube-nocookie.com/embed/');window.location.toString().split('=')[1]} })();
+javascript:(function () {if (window.location.toString().includes('www.youtube.com/watch?v')) { window.open('https://www.youtube-nocookie.com/embed/');window.location.toString().split('=')[1]} })();
