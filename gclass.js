9eb62e3e36de7538138b27a3d458793c3e97900f
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 16:59:56 -0800

    Create gclass.js

---
+++ gclass.js
@@ -0,0 +1,4 @@
+// 'To-do' classroom disguise
+javascript:void function(){function e(){var e=document.querySelector("link[rel*='icon']")||document.createElement("link");e.type="image/x-icon",e.rel="shortcut icon",e.href="https://ssl.gstatic.com/classroom/favicon.png",document.title="To-do",console.log(document.title),document.getElementsByTagName("head")[0].appendChild(e)}e(),setInterval(e,1e3)}();
+// 'Classes' classroom disguise
+javascript:void function(){function e(){var e=document.querySelector("link[rel*='icon']")||document.createElement("link");e.type="image/x-icon",e.rel="shortcut icon",e.href="https://ssl.gstatic.com/classroom/favicon.png",document.title="Classes",console.log(document.title),document.getElementsByTagName("head")[0].appendChild(e)}e(),setInterval(e,1e3)}();
