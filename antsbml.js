62a398d25104949e356a97821cfb4261f1373e74
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2022-12-12 16:49:52 -0800

    Update antsbml.js

--- antsbml.js
+++ antsbml.js
@@ -1,3 +1,1 @@
-var e=document.createElement("script");
-e.src="https://www.cs.huji.ac.il/~irush/scripts/antsbml.js";
-document.getElementsByTagName("head")[0].appendChild(e);
+javascript:void function(){var e=document.createElement("script");e.src="https://www.cs.huji.ac.il/~irush/scripts/antsbml.js",document.getElementsByTagName("head")[0].appendChild(e)}();
